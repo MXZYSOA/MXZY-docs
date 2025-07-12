f1af5a99dcd06c5fd1010b80effd373c244980e1
lost-sky <89430129+creeperCN@users.noreply.github.com>
2025-07-12 12:27:14 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,22 +4,22 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '星域世界文档库',
+  title: '冒险之源文档库',
   head: [
-    ['link', { rel: 'canonical', href: 'https://help.mcstaralliance.com/' }],
+    ['link', { rel: 'canonical', href: 'https://help.mxzysoa.top' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
-    ['meta', { name: 'description', content: '星域世界的官方文档库' }],
-    ['meta', { name: 'keywords', content: '我的世界, Minecraft, 星域世界, 文档, 游戏' }],
+    ['meta', { name: 'description', content: '冒险之源的官方文档库' }],
+    ['meta', { name: 'keywords', content: '我的世界, Minecraft, 冒险之源, 文档, 游戏' }],
   ],
   theme: defaultTheme({
     navbar: [
       {
-        text: 'Celestia Skin',
-        link: 'https://celestia.skin/',
+        text: 'MXZY Skin',
+        link: 'https://skin.mxzysoa.top/',
       },
       {
         text: '鸣谢名单',
-        link: 'https://help.mcstaralliance.com/acknowledgments',
+        link: 'https://help.mxzysoa.top/acknowledgments',
       }
     ],
     sidebar: [
